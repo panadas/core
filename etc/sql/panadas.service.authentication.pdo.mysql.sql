4881705a71aa68c3ce5236df2c69f5d758d3e08c
Adam Pancutt <adam@panadas.co.uk>
2014-01-22 19:02:52 +0000

    Removed unrequired files

--- etc/sql/panadas.service.authentication.pdo.mysql.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE `authentication` (
-    `token` VARCHAR(40) COLLATE utf8_unicode_ci NOT NULL,
-    `user_id` INTEGER UNSIGNED NOT NULL,
-    `lifetime` INTEGER UNSIGNED,
-    `created` DATETIME NOT NULL,
-    `modified` DATETIME NOT NULL,
-    PRIMARY KEY (`token`),
-    KEY `idx_modified` (`modified`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
