750f4759a3f4b01b662c2e85ca5f6bd99a1a6b2b
Adam Pancutt <adam@panadas.co.uk>
2014-01-28 20:02:03 +0000

    Removed JsonSerializable and Serializable interfaces

--- src/Panadas/AbstractBase.php
+++ src/Panadas/AbstractBase.php
@@ -1,7 +1,7 @@
 <?php
 namespace Panadas;
 
-abstract class AbstractBase implements \JsonSerializable, \Serializable
+abstract class AbstractBase
 {
 
     /**
@@ -17,42 +17,5 @@ abstract class AbstractBase implements \JsonSerializable, \Serializable
     public function __toString()
     {
         return "";
-    }
-
-    /**
-     * @return array
-     */
-    public function __toArray()
-    {
-        return [];
-    }
-
-    /**
-     * @return array
-     */
-    public function jsonSerialize()
-    {
-        return $this->__toArray();
-    }
-
-    /**
-     * @return string
-     */
-    public function serialize()
-    {
-        return serialize($this->__toArray());
-    }
-
-    /**
-     * @param  string $serialized
-     * @return \Panadas\AbstractBase
-     */
-    public function unserialize($serialized)
-    {
-        foreach (unserialize($serialized) as $key => $value) {
-            $this->$key = $value;
-        }
-
-        return $this;
     }
 }
